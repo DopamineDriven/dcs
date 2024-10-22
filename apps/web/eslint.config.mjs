fc9921ab2000d060b401c5767a869c9b99030a9c
Andrew Ross <andrew.ross@takeda.com>
2024-10-22 17:06:09 -0500

    stash

---
+++ apps/web/eslint.config.mjs
@@ -0,0 +1,17 @@
+import baseConfig from "@dcs/eslint-config/base";
+import nextjsConfig from "@dcs/eslint-config/next";
+import reactConfig from "@dcs/eslint-config/react";
+
+/** @type {import('typescript-eslint').Config} */
+export default [
+  ...baseConfig,
+  ...reactConfig,
+  ...nextjsConfig,
+  {
+    ignores: [".next/**", "!.next/types/**/*"],
+    rules: {
+      "@typescript-eslint/consistent-type-assertions": "off",
+      "@typescript-eslint/require-await": "off"
+    }
+  }
+];
