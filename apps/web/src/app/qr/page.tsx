3f5734c5b505eaa7584a5ed31fd8ad9880afe97d
Andrew Ross <andrew.ross@takeda.com>
2024-11-07 15:00:40 -0600

    optimizing

--- apps/web/src/app/qr/page.tsx
+++ apps/web/src/app/qr/page.tsx
@@ -1,14 +1,10 @@
 import type { Metadata } from "next";
-import { Suspense } from "react";
 import { ParticleHeaderComponent } from "@/components/particle-header";
-import { Spinner } from "@/ui/loading/Spinner";
-import { MetaHandler } from "@/ui/meta/Swr";
 import { EventForm } from "@/ui/sections/EventForm";
 
 export const metadata = {
   title: "Event"
 } satisfies Metadata;
-
 
 export default function QrPage() {
   return (
@@ -22,14 +18,7 @@ export default function QrPage() {
         <div className='absolute inset-0 overflow-hidden' />
       </div>
       <div className='relative'>
-        <Suspense fallback={<Spinner />}>
-          <EventForm />
-        </Suspense>
-      </div>
-      <div className='relative mx-auto w-screen justify-center'>
-        <Suspense>
-          <MetaHandler />
-        </Suspense>
+        <EventForm />
       </div>
     </>
   );
