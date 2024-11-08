0fca8153e71fc6888bf831a530297dc04f894ba8
Andrew Ross <andrew.ross@takeda.com>
2024-11-08 11:32:58 -0600

    optimizing

---
+++ apps/web/src/ui/dynamic/OurExpertise.tsx
@@ -0,0 +1,90 @@
+import { cn } from "@/lib/utils";
+import { toHex } from "@/utils/to-hex";
+
+export const services = [
+  {
+    service: "CRM System Integrations"
+  },
+  {
+    service: "Enterprise C-Level Relations"
+  },
+  {
+    service: "Implementation & Support"
+  },
+  {
+    service: "SaaS & Cloud Based Services"
+  },
+  {
+    service: "Configuration Services"
+  },
+  {
+    service: "Change Control"
+  },
+  {
+    service: "Project Management"
+  },
+  {
+    service: "Training Modeling & Delivery"
+  },
+  {
+    service: "System Monitoring"
+  },
+  {
+    service: "Business Process Modeling"
+  },
+  {
+    service: "HCM Applications"
+  },
+  {
+    service: "Budget Creation / Tracking"
+  },
+  {
+    service: "Vendor Management"
+  },
+  {
+    service: "Process Design & Refinement"
+  },
+  {
+    service: "P & L / Cost Benefit Analysis"
+  },
+  {
+    service: "Goal Acquisition"
+  },
+  {
+    service: "services Mining & Analytics"
+  },
+  {
+    service: "Requirements Elicitation"
+  },
+  {
+    service: "System Administration"
+  }
+];
+
+export function OurExpertise() {
+  return (
+    <div className='mx-auto grid max-w-2xl grid-cols-1 items-start gap-x-8 lg:mx-0 lg:max-w-none lg:grid-cols-2'>
+      <div className='mx-auto mt-2 text-base/7 text-gray-700 lg:col-span-2'>
+        <h1 className='text-pretty font-basis-grotesque-pro-medium text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl'>
+          {"Our Expertise"}
+        </h1>
+      </div>
+      <div className='col-span-2'>
+        <dl className='mt-2 grid grid-cols-1 gap-0 sm:grid-cols-2 lg:grid-cols-3'>
+          {services.map((item, i) => (
+            <div
+              key={toHex(item.service)}
+              className={cn(
+                "overflow-hidden bg-dcs-50 px-4 py-5 shadow-magical sm:p-6",
+                ++i === services.length ? "sm:col-span-2 lg:col-span-3" : ""
+              )}>
+              <dt className='text-center font-basis-grotesque-pro-medium text-xl tracking-tight text-gray-900'>
+                {item.service}
+              </dt>
+            </div>
+          ))}
+        </dl>
+      </div>
+    </div>
+  );
+}
--- apps/web/src/ui/dynamic/Switch.tsx
+++ apps/web/src/ui/dynamic/Switch.tsx
@@ -3,8 +3,9 @@
 import Image from "next/image";
 import type { PageByUriProps } from "@/types/wp";
 import { shimmer } from "@/lib/shimmer";
+import { ChildrenScaffold } from "@/ui/dynamic/ChildrenScaffold";
+import { OurExpertise } from "@/ui/dynamic/OurExpertise";
 import { SubmitResume } from "@/ui/sections/SubmitResumeForm";
-import { ChildrenScaffold } from "../dynamic/ChildrenScaffold";
 
 export type SubContentSwitchProps = { pathname: string } & PageByUriProps;
 
@@ -13,7 +14,7 @@ export function SubContentSwitch({ pathname, page }: SubContentSwitchProps) {
     case "/about-us/our-project-delivery": {
       return (
         <div className='mx-auto grid max-w-2xl grid-cols-1 items-start gap-x-8 gap-y-16 sm:gap-y-24 lg:mx-0 lg:max-w-none lg:grid-cols-2'>
-          <div className='my-2 mx-auto text-base/7 text-gray-700 lg:col-span-2'>
+          <div className='mx-auto my-2 text-base/7 text-gray-700 lg:col-span-2'>
             <h1 className='text-pretty font-basis-grotesque-pro-medium text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl'>
               {page.title}
             </h1>
@@ -27,8 +28,8 @@ export function SubContentSwitch({ pathname, page }: SubContentSwitchProps) {
               height='1707'
               blurDataURL={shimmer({ w: 2132, h: 1707 })}
               placeholder='blur'
-              style={{objectFit: "cover"}}
-              loading="eager"
+              style={{ objectFit: "cover" }}
+              loading='eager'
             />
           </div>
         </div>
@@ -38,6 +39,13 @@ export function SubContentSwitch({ pathname, page }: SubContentSwitchProps) {
       return (
         <div className='relative'>
           <SubmitResume />
+        </div>
+      );
+    }
+    case "/about-us/our-expertise": {
+      return (
+        <div className='relative'>
+          <OurExpertise />
         </div>
       );
     }
