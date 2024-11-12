6158096eacbb034f7748bfe27004669ef7cc3bf9
Andrew Ross <andrew.ross@takeda.com>
2024-11-12 15:31:26 -0600

    verification

--- apps/web/next-sitemap.mjs
+++ apps/web/next-sitemap.mjs
@@ -8,27 +8,18 @@ const localUrl = "http://localhost:3001";
  * @param {"development" | "production" | "test" | undefined} env
  */
 const getSiteUrl = env =>
-  process.env.VERCEL_ENV === "development"
-    ? previewUrl
-    : !env || env === "development"
-      ? localUrl
-      : process.env.VERCEL_ENV === "production"
-        ? productionUrl
+  process.env.VERCEL_ENV === "production"
+    ? productionUrl
+    : process.env.VERCEL_ENV === "preview"
+      ? previewUrl
+      : !env || env === "development"
+        ? localUrl
         : previewUrl;
-
-/**
- * @param {ReturnType<typeof getSiteUrl>} siteUrl
- * @returns {"https://drisdell-dev.vercel.app" | "https://drisdell.com" | "http://localhost:3001" | `https://${string}`}
- */
-const flexiUrl = siteUrl =>
-  process.env.VERCEL_URL && process.env.VERCEL_URL !== siteUrl
-    ? `https://${process.env.VERCEL_URL}`
-    : siteUrl;
 
 // @ts-check
 /** @type {import('next-sitemap').IConfig} */
 export default {
-  siteUrl: flexiUrl(getSiteUrl(process.env.NODE_ENV)),
+  siteUrl: getSiteUrl(process.env.NODE_ENV),
   changefreq: "daily",
   priority: 0.9,
   exclude: ["/_*"],
